af31114c807e2153872f0dc71a354aacd0dab933
Zach Delgros <68976965+Raisinaut@users.noreply.github.com>
2023-09-18 21:09:43 -0700

    Swapped touchscreen joystick for discrete movement buttons + moved skip button

--- Fighting Ghosts in Another World/Godot/UI/Onscreen Controls/TouchControls.gd
+++ Fighting Ghosts in Another World/Godot/UI/Onscreen Controls/TouchControls.gd
@@ -2,8 +2,7 @@ extends CanvasLayer
 
 
 
-onready var regionRight = $RegionRight
-onready var regionLeft = $RegionLeft
+onready var gestureRegion = $GestureRegion
 var right_press_pos : Vector2
 
 var enabled = false setget set_enabled
@@ -12,18 +11,20 @@ var enabled = false setget set_enabled
 
 func _ready():
 	self.enabled = OS.has_feature("mobile")
+	$GestureMenu.set_menu_active(false)
 
 
 func set_enabled(state):
 	enabled = state
-	$Joystick.enabled = enabled
 	$GestureMenu.menu_active = enabled
 
 
 func _process(_delta):
 	var d = Global.dialog_is_active
 	var s = SceneChanger.scene_is_changing
+	var t = Global.throw_tutorial_active
 	visible = not d and not s
+	$MovementButtons.visible = not t
 
 
 func _input(event):
@@ -35,24 +36,13 @@ func _input(event):
 	
 	if event is InputEventScreenTouch:
 		# Right side touch
-		if point_in_region(event.position, regionRight):
+		if point_in_region(event.position, gestureRegion):
 			if event.is_pressed() and $GestureMenu.menu_active == false:
 				right_press_pos = event.position
 				$GestureMenu.position = right_press_pos
 				$GestureMenu.set_menu_active(true)
 			else:
 				$GestureMenu.set_menu_active(false)
-		
-		# Left side touch
-		if point_in_region(event.position, regionLeft):
-				if event.is_pressed():
-					$Joystick.set_stick_position(event.position)
-				else:
-					$Joystick.reset_stick()
-	
-	if event is InputEventScreenDrag:
-		if point_in_region(event.position, regionLeft):
-			$Joystick.set_stick_position(event.position)
 
 func point_in_region(pos : Vector2, region : TouchScreenButton) -> bool:
 	var region_rect = Rect2(region.position, region.shape.extents * 2)
